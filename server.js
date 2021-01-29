9ae42221e309523450d6a3e8047646e9bab1d14b
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-01-29 10:46:35 -0500

    created routes folder and neccessary files and folders to define routes and import it in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
@@ -13,6 +14,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
