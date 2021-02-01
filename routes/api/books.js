9d407903d829dcd4212f1642065fc4a763fe02dc
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-02-01 12:09:57 -0500

    defined endpoint for finding specifc book by its id, update the existing book record, delete the the book from database

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,3 +3,12 @@ const bookController = require("../../controllers/bookController");
 
 // endpoint "/api/books" to find all books
 router.route("/").get(bookController.findAll).post(bookController.create);
+
+// endpoint "/api/books/:id"
+router
+  .route("/:id")
+  .get(bookController.findById)
+  .put(bookController.update)
+  .delete(bookController.remove);
+
+module.exports = router;
