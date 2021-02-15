fadeec3cd90f876849c408454a0abb74e45fafa0
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-02-15 17:35:12 -0500

    added Search button

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Button } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "30ch",
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 function SearchBar() {
+  const classes = useStyles();
+
   return (
     <div>
-      <p>SearchBar</p>
+      {/* <form className={classes.root} noValidate autoComplete="off"> */}
+      <TextField
+        id="outlined-basic"
+        label="Search for book"
+        variant="outlined"
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        className={classes.button}
+        startIcon={<SaveIcon />}
+      >
+        Search
+      </Button>
+      {/* </form> */}
     </div>
   );
 }
