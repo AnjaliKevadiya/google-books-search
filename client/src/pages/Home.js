a53baae63e1bdfe4bc1dad206efc8c252840d3a6
Anjali Kevadiya <angeldungrani5@gmail.com>
2021-02-23 18:41:17 -0500

    get all the books using API on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
+import API from "../utils/API";
 
 function Home() {
+  function loadPosts() {
+    API.getBooks()
+      .then((res) => {
+        console.log("books", res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  loadPosts();
   return (
     <div>
       <Header />
